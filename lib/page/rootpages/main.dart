1e7b6ba161aa010423597b2e6acecd16d5b889fb
rycont <reactdev@kakao.com>
2021-04-15 07:45:47 +0900

    feat : add bottom navigation routing

--- lib/page/rootpages/main.dart
+++ lib/page/rootpages/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:dimigoin/component/bottomNavBar.dart';
+import '../../component/bottomNavBar.dart';
+import './home.dart';
+import './meal.dart';
 
 class RootPages extends StatefulWidget {
   RootPages({Key key}) : super(key: key);
@@ -9,15 +11,19 @@ class RootPages extends StatefulWidget {
 
 class _RootPagesState extends State<RootPages> {
   int pageIndex = 0;
+  List<Widget> rootPages = [
+    HomePage(),
+    MealPage()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavBar(
-        currentPageIndex: pageIndex,
-        onIndexChanged: (index) => {
-          setState(() => {pageIndex = index})
-        }),
-      body: Text('$pageIndex번 페이지'),
+          currentPageIndex: pageIndex,
+          onIndexChanged: (index) => {
+                setState(() => {pageIndex = index})
+              }),
+      body: rootPages[pageIndex],
     );
   }
 }
